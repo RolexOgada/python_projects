6fd2f9a8a290f13c8959ac33514da57ec5c746b4
RolexOgada <rolexogada@gmail.com>
2021-10-25 21:52:51 +0300

    added a function

--- countingNumbers.py
+++ countingNumbers.py
@@ -1,5 +1,12 @@
-print("We'll count numbers from one to ten")
-counter = 1
-while counter <= 10:
-    print(counter)
-    counter += 1
+def countingNumbers(num):
+    counter = 1 
+      
+    while counter <= num:
+        print(counter)
+        counter += 1
+    return
+
+usernumber = int(input("Enter the last number: "))
+countingNumbers(usernumber)
+
+
