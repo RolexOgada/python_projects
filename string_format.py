f2712b8917b5b5e5aea84769349c9af56721de9a
RolexOgada <rolexogada@gmail.com>
2021-10-29 16:10:02 +0300

    added py

---
+++ string_format.py
@@ -0,0 +1,3 @@
+year = 2
+txt = "i have {} years of experience in manufucturing industry"
+print(txt.format(year))
---
+++ var_insidefunc.py
@@ -0,0 +1,11 @@
+y="option"
+
+def myfunc():
+  y="safaricom"
+  print(" The best simcard line is "+ y)
+
+myfunc()
+
+print(" The better "+ y)
+
+ 
