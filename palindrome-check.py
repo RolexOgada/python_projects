b5e4c15fbba0e3d88687624e02b2a32cce9a4481
OgadaOgada <collinceogada@gmail.com>
2021-10-30 09:59:39 +0300

    changed palindrome to function

--- palindrome-check.py
+++ palindrome-check.py
@@ -1,23 +1,22 @@
 #CHECKING A PALINDROME
 #a number that if reversed remains the same e.g mum
 
+def userinput(text):
+    num_length = len(text)
+    lastindex = -1
+    reversedText = ""
+
+    while num_length !=0:
+        reversedText = reversedText + text[lastindex]
+        num_length -= 1
+        lastindex -= 1
+
+    if reversedText == text:
+        print("That is a palindrome!\n")
+    else:
+        print("That's not a palindrome!\n")
+
+
+
 user_input = input("\nEnter a text or number: ")
-num_length = len(user_input)
-firstindex = 0
-lastindex = -1
-palindromeCheck = True
-
-while  firstindex < num_length/2:
-    rightchars = user_input[firstindex]
-    leftchars = user_input[lastindex]
-
-    if rightchars != leftchars:
-        palindromeCheck = False
-
-    firstindex += 1
-    lastindex -= 1
-
-if palindromeCheck:
-    print("That is a palindrome!\n")
-else:
-    print("That's not a palindrome!\n")
+userinput(user_input)
