406dc91d37b2f712327dc9833caf68ede6c31318
RolexOgada <rolexogada@gmail.com>
2021-11-01 22:22:23 +0300

    new py

---
+++ ListAsArg.py
@@ -0,0 +1,7 @@
+def my_function(car):
+  for x in car:
+    print(x)
+
+car = ["mercedes", "mazda", "probox"]
+
+my_function(car)
---
+++ return_value.py
@@ -0,0 +1,6 @@
+def my_function(y):
+  return 7 * y
+
+print(my_function(12))
+print(my_function(10))
+print(my_function(20))
