5ef50c7f2d5b657f81f6506e21167e43d691c821
OgadaOgada <collinceogada@gmail.com>
2021-12-21 17:44:11 +0300

    untracked changes

--- copyPaste.py
+++ copyPaste.py
@@ -1,8 +1,16 @@
-import pyperclip
-text = pyperclip.paste() #pasting the content of copied text
-print(text)
+import pyperclip,re
+import os
 
-name = input("Enter your name ")
-age = input("Enter your age ")
-you = "my name is "+name+" and am "+age+" years old"
-pyperclip.copy(you) #copying the content of variable you
+phone_regex = re.compile(r"\d\d\d-\d\d\d-\d\d\d\d")
+email_regex = re.compile(r"[a-zA-Z0-9_+.]+@[a-zA-Z09_+.]+")
+text = pyperclip.paste()
+matched_phone = phone_regex.findall(text)
+matched_email = email_regex.findall(text)
+
+for i in matched_email:
+    print(i)
+for i in matched_phone:
+    print(i)
+
+# print("\\")
+# print(r"\\")
